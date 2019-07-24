351e7d079bc2e0bee08fed8708013880fe7ef1da
Jema <be.jema.kreys@gmail.com>
2019-07-24 12:34:50 +0100

    talk to doctor module added

--- Main.py
+++ Main.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, request, session, url_for, redirect
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.secret_key = 'whoknowsthissecretw'
+socketio = SocketIO(app)
+
+''' This is a list of all variables
+    session['login'] = True
+    session['user'] = request.form['user']
+    session['gender'] = request.form['gender']
+    session['age'] = request.form['age']
+    session['obesity'] = request.form['optionsWeight']
+    session['cigar'] = request.form['optionsCigarettes']
+    session['chol'] = request.form['optionsCholesterol']
+    session['hypert'] = request.form['optionsHypertension']
+    session['diabetes'] = request.form['optionsDiabetes']
+    session['illRange'] = request.form['optionsRange']
+    session['duration']  = request.form['optionsDuration']
+    session['trauma']  = request.form['optionsTrauma']
+    session['fever']  = request.form['optionsFever']
+
+'''
 
 # routing functions
 @app.route('/')
@@ -31,6 +50,15 @@ def symptome1():
 
 @app.route('/symptome2', methods=['GET', 'POST'])
 def symptome2():
+    try:
+        session['obesity'] = request.form['optionsWeight']
+        session['cigar'] = request.form['optionsCigarettes']
+        session['chol'] = request.form['optionsCholesterol']
+        session['hypert'] = request.form['optionsHypertension']
+        session['diabetes'] = request.form['optionsDiabetes']
+    except:
+        pass
+
     return render_template('symptome2.html')
 
 @app.route('/symptome3', methods=['GET', 'POST'])
@@ -39,20 +67,95 @@ def symptome3():
 
 @app.route('/symptome4', methods=['GET', 'POST'])
 def symptome4():
+    try:
+        session['illRange'] = request.form['optionsRange']
+    except:
+        pass
+
+    print(session['illRange'])
     return render_template('symptome4.html')
 
 @app.route('/symptome5', methods=['GET', 'POST'])
 def symptome5():
+    try:
+        session['duration']  = request.form['optionsDuration']
+    except:
+        pass
+
     return render_template('symptome5.html')
 
 @app.route('/symptome6', methods=['GET', 'POST'])
 def symptome6():
+    try:
+        session['trauma']  = request.form['optionsTrauma']
+    except:
+        pass
+
     return render_template('symptome6.html')
 
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    return render_template('results.html')
+    try:
+        session['fever']  = request.form['optionsFever']
+    except:
+        pass
 
+
+    return render_template('results.html', user = session['user'], gender = session['gender'], age = session['age'], obesity = session['obesity'], cigar = session['cigar'], chol = session['chol'], hypert = session['hypert'], diabetes = session['diabetes'], illRange = session['illRange'], duration = session['duration'],  trauma = session['trauma'],  fever = session['fever'] )
+
+
+# Routes for first aid pages
+@app.route('/firstAid')
+def firstAid():
+    return render_template('first-aid.html')
+
+# bleeding pages
+@app.route('/bleeding_1')
+def bleeding_1():
+    return render_template('bleeding/bleeding_1.html')
+@app.route('/bleeding_2')
+def bleeding_2():
+    return render_template('bleeding/bleeding_2.html')
+@app.route('/bleeding_3')
+def bleeding_3():
+    return render_template('bleeding/bleeding_3.html')
+@app.route('/bleeding_4')
+def bleeding_4():
+    return render_template('bleeding/bleeding_4.html')
+@app.route('/bleeding_5')
+def bleeding_5():
+    return render_template('bleeding/bleeding_5.html')
+@app.route('/bleeding_6')
+def bleeding_6():
+    return render_template('bleeding/bleeding_6.html')
+
+# cpr pages
+@app.route('/cpr_1')
+def cpr_1():
+    return render_template('cpr/cpr_1.html')
+@app.route('/cpr_2')
+def cpr_2():
+    return render_template('cpr/cpr_2.html')
+@app.route('/cpr_3')
+def cpr_3():
+    return render_template('cpr/cpr_3.html')
+@app.route('/cpr_4')
+def cpr_4():
+    return render_template('cpr/cpr_4.html')
+@app.route('/cpr_5')
+def cpr_5():
+    return render_template('cpr/cpr_5.html')
+@app.route('/cpr_6')
+def cpr_6():
+    return render_template('cpr/cpr_6.html')
+
+@app.route('/doctor')
+def doctor():
+    return render_template('doctor.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/logout')
 def logout():
@@ -69,6 +172,15 @@ def ajaxcall():
 def page_not_found(error):
     return render_template('error.html'), 404
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    # print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,33 @@
 
 // $('.options-btn').on('click').toggleClass('selected')
 $(function () { $('.popover-toggle').popover('toggle');});
+
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+let user_name = null;
+let user_input = 'connected'
+socket.on( 'connect', function() {
+  socket.emit( 'my event', {
+    data: 'User Connected'
+  } )
+  var form = $( 'form' ).on( 'submit', function( e ) {
+    e.preventDefault()
+    if (user_name === null) {
+      user_name = $( 'input.username' ).val()
+      $( 'input.username' ).hide()
+    }
+
+    user_input = $( 'input.message' ).val()
+    socket.emit( 'my event', {
+      user_name : user_name,
+      message : user_input
+    } )
+    $( 'input.message' ).val( '' ).focus()
+  } )
+} )
+socket.on( 'my response', function( msg ) {
+  console.log( msg )
+  if( typeof msg.user_name !== 'undefined' ) {
+    $( 'h3' ).remove()
+    $( 'div.message_holder' ).append( '<div><b style="color: #000">'+msg.user_name+'</b> '+msg.message+'</div>' )
+  }
+})
